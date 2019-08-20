454a80d7f8a89d12e6a90daf2701ac18974eb85e
Morgan Chen <morganchen12@gmail.com>
2019-08-20 11:16:08 -0700

    Initial commit

---
+++ FirestorePatterns/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirestorePatterns
+//
+//  Created by Morgan Chen on 8/20/19.
+//  Copyright © 2019 Firebase. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
